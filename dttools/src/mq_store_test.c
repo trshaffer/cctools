e9a2c7d8b371fdb754a57e992e9f21c6559496e6
Tim Shaffer <tshaffe1@nd.edu>
2020-09-21 13:36:20 -0400

    test

--- dttools/src/mq_store_test.c
+++ dttools/src/mq_store_test.c
@@ -11,6 +11,37 @@
 #include "buffer.h"
 #include "xxmalloc.h"
 
+#include <stdio.h>
+
+void DumpHex(const void* data, size_t size) {
+	char ascii[17];
+	size_t i, j;
+	ascii[16] = '\0';
+	for (i = 0; i < size; ++i) {
+		printf("%02X ", ((unsigned char*)data)[i]);
+		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+			ascii[i % 16] = ((unsigned char*)data)[i];
+		} else {
+			ascii[i % 16] = '.';
+		}
+		if ((i+1) % 8 == 0 || i+1 == size) {
+			printf(" ");
+			if ((i+1) % 16 == 0) {
+				printf("|  %s \n", ascii);
+			} else if (i+1 == size) {
+				ascii[(i+1) % 16] = '\0';
+				if ((i+1) % 16 <= 8) {
+					printf(" ");
+				}
+				for (j = (i+1) % 16; j < 16; ++j) {
+					printf("   ");
+				}
+				printf("|  %s \n", ascii);
+			}
+		}
+	}
+}
+
 int main (int argc, char *argv[]) {
 	const char *string1 = "test message";
 
@@ -110,6 +141,7 @@ int main (int argc, char *argv[]) {
 	assert(rc == 1);
 	rc = mq_recv(client, &got_len);
 	assert(rc == MQ_MSG_BUFFER);
+	DumpHex(buffer_tostring(&got_string), got_len);
 	assert(got_len == 10);
 
 	srcfd = open(argv[0], O_RDONLY);
